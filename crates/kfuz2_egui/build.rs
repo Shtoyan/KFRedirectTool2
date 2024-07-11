9555215bde55e4a4589b8f2de74821f6ca288c53
Shtoyan <shtoyan@proton.me>
2024-07-11 18:32:07 +0400

    Update build.rs

--- crates/kfuz2_egui/build.rs
+++ crates/kfuz2_egui/build.rs
@@ -1,6 +1,7 @@
 // ref: // ref: https://stackoverflow.com/questions/30291757/attaching-an-icon-resource-to-a-rust-application/65393488#65393488
 use std::{env, io};
 
+#[cfg(target_family = "windows")]
 fn main() -> io::Result<()> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         use winres::WindowsResource;
@@ -11,4 +12,9 @@ fn main() -> io::Result<()> {
             .compile()?;
     }
     Ok(())
+}
+
+#[cfg(target_family = "unix")]
+fn main() {
+    println!("aaaa");
 }
