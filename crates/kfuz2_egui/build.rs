648798f9da3d28660dc563e26dbfadd692cd5be0
Shtoyan <shtoyan@proton.me>
2024-07-11 18:25:38 +0400

    test bld rs

--- crates/kfuz2_egui/build.rs
+++ crates/kfuz2_egui/build.rs
@@ -1,9 +1,10 @@
 // ref: // ref: https://stackoverflow.com/questions/30291757/attaching-an-icon-resource-to-a-rust-application/65393488#65393488
 use std::{env, io};
-use winres::WindowsResource;
 
 fn main() -> io::Result<()> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        use winres::WindowsResource;
+
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
             .set_icon("src//static//icon.ico")
